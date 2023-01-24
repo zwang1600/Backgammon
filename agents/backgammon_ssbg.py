f0f6ae4bccda360f5b4db3f89f204e3e7c0082e9
zwang1600 <zwang1600@gmail.com>
2023-01-23 21:15:04 -0800

    Initial Commit

---
+++ agents/backgammon_ssbg.py
@@ -0,0 +1,44 @@
+'''
+Name(s):
+UW netid(s):
+'''
+
+from game_engine import genmoves
+
+class BackgammonPlayer:
+    def __init__(self):
+        self.GenMoveInstance = genmoves.GenMoves()
+        # feel free to create more instance variables as needed.
+
+    # TODO: return a string containing your UW NETID(s)
+    # For students in partnership: UWNETID + " " + UWNETID
+    def nickname(self):
+        # TODO: return a string representation of your UW netid(s)
+        return ""
+
+    # Given a ply, it sets a maximum for how far an agent
+    # should go down in the search tree. Count the chance nodes
+    # as a ply too!
+    def setMaxPly(self, maxply=2):
+        # TODO: set the max ply
+        pass
+
+    # If not None, it update the internal static evaluation
+    # function to be func
+    def useSpecialStaticEval(self, func):
+        # TODO: update your staticEval function appropriately
+        pass
+
+    # Given a state and a roll of dice, it returns the best move for
+    # the state.whose_move
+    # Keep in mind: a player can only pass if the player cannot move any checker with that role
+    def move(self, state, die1, die2):
+        # TODO: return a move for the current state and for the current player.
+        # Hint: you can get the current player with state.whose_move
+        return 'q'
+
+
+    # Hint: Look at game_engine/boardState.py for a board state properties you can use.
+    def staticEval(self, state):
+        # TODO: return a number for the given state
+        return -1
